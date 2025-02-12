c544ce9771d20d9605ecb8f7af383c46fba0a25a
oscilococcinum <gawlikbar@gmail.com>
2025-02-13 00:35:46 +0100

    moves src url to flake

--- flake.nix
+++ flake.nix
@@ -1,17 +1,21 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devdrop = {
+      url = "https://github.com/oscilococcinum/openhantek6022-nix/releases/download/devdrop/openhantek_devdrop_x86_64.tar.gz";
+      flake = false;
+    };
   };
 
   outputs = { nixpkgs, ... }@inputs: {
-    packages = builtins.listToAttrs (map (system:
-      {
-        name = system;
-        value = with import nixpkgs { inherit system; config.allowUnfree = true;}; rec {
+    packages = builtins.listToAttrs (map (system: {
+      name = system;
+      value = with import nixpkgs { inherit system; config.allowUnfree = true;}; rec {
 
-          openhantek6022 = pkgs.callPackage (import ./nix) { };
-        };
-      }
-    )[ "x86_64-linux" ]);
+        openhantek6022 = pkgs.callPackage (import ./nix) { repo = inputs.devdrop; };
+
+        default = openhantek6022;
+      };
+    })[ "x86_64-linux" ]);
   };
 }
--- nix/default.nix
+++ nix/default.nix
@@ -2,17 +2,15 @@
   stdenv,
   autoPatchelfHook,
   libsForQt5,
-  pkgs
+  pkgs,
+  repo
 }:
 
 stdenv.mkDerivation rec {
   pname = "openhantek";
   version = "devdrop";
 
-  src = builtins.fetchTarball {
-    url = "https://github.com/oscilococcinum/openhantek6022-nix/releases/download/devdrop/openhantek_devdrop_x86_64.tar.gz";
-    sha256 = "sha256:0dpv50ky4lpva8p1q05mffk2ak2yk97i3g01kp8miq71rlpny5w6";
-  };
+  src = repo;
 
   nativeBuildInputs = [
     autoPatchelfHook
@@ -29,5 +27,7 @@ stdenv.mkDerivation rec {
   installPhase = ''
     mkdir -p $out/bin
     cp -r $src/* $out/
+    cp $out/bin/OpenHantek $out/bin/openhantek
+    rm $out/bin/OpenHantek
   '';
 }
