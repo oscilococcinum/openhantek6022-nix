82bbcd572249bf15ff1e815a636021e63db054fc
oscilococcinum <gawlikbar@gmail.com>
2025-03-04 23:29:04 +0100

    updates nix package to current devdrop-3.4-rc4-1

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     devdrop = {
-      url = "https://github.com/oscilococcinum/openhantek6022-nix/releases/download/devdrop/openhantek_devdrop_x86_64.tar.gz";
+      url = "https://github.com/oscilococcinum/openhantek6022-nix/releases/download/devdrop-3.4-rc4-1/openhantek_devdrop_x86_64.tar.gz";
       flake = false;
     };
   };
@@ -11,9 +11,7 @@
     packages = builtins.listToAttrs (map (system: {
       name = system;
       value = with import nixpkgs { inherit system; config.allowUnfree = true;}; rec {
-
         openhantek6022 = pkgs.callPackage (import ./nix) { src = inputs.devdrop; };
-
         default = openhantek6022;
       };
     })[ "x86_64-linux" ]);
