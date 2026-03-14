a523aca28b2c9671ba410ff8abef788f855a4d26
oscilococcinum <bargaw28@gmail.com>
2026-03-14 09:29:00 +0100

    updates openhantek to 3.4.0 stable

--- flake.nix
+++ flake.nix
@@ -1,19 +1,27 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    devdrop = {
-      url = "https://github.com/OpenHantek/OpenHantek6022/releases/download/devdrop/openhantek_3.4.0-1-g982f746-devdrop_x86_64.tar.gz";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    stable = {
+      url = "https://github.com/OpenHantek/OpenHantek6022/releases/download/3.4.0/openhantek_3.4.0_x86_64.tar.gz";
       flake = false;
     };
   };
 
-  outputs = { nixpkgs, ... }@inputs: {
-    packages = builtins.listToAttrs (map (system: {
-      name = system;
-      value = with import nixpkgs { inherit system; config.allowUnfree = true;}; rec {
-        openhantek6022 = pkgs.callPackage (import ./package/default.nix) { src = inputs.devdrop; };
-        default = openhantek6022;
-      };
-    })[ "x86_64-linux" ]);
-  };
+  outputs =
+    { nixpkgs, ... }@inputs:
+    {
+      packages = builtins.listToAttrs (
+        map (system: {
+          name = system;
+          value =
+            with import nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            }; rec {
+              openhantek6022 = pkgs.callPackage (import ./package/default.nix) { src = inputs.stable; };
+              default = openhantek6022;
+            };
+        }) [ "x86_64-linux" ]
+      );
+    };
 }
